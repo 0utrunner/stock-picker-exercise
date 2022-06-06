627f9e7d476c027adf2276c49b10727b32537ea9
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-06-06 16:32:46 -0500

    SOLVED

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,27 @@
 exports.picker = function(prices) {
+    let graph = prices.slice()
+    let bestDays = []
+    let buyDay = graph[0]
+    let bestBuyDay = []
+    let sellDay = graph[1]
+    let moneyMade = sellDay - buyDay
+    let counter = 0
 
+    while(graph.length != 0){
+        while(counter < graph.length){
+            if(graph[0] - buyDay > moneyMade){
+                bestBuyDay = buyDay
+                sellDay = graph[0]
+                moneyMade = graph[0] - buyDay
+            }
+            graph.push(graph.shift())
+            counter++
+        }
+        buyDay = graph.shift()
+        counter = 0
+    }
+    bestDays.push(prices.indexOf(bestBuyDay))
+    bestDays.push(prices.indexOf(sellDay))
+
+    return bestDays
 }
--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,23 @@
 def picker(prices):
-    pass
+    graph = prices.copy()
+    best_days = []
+    buy_day = graph[0]
+    best_buy_day = []
+    sell_day = graph[1]
+    money_made = sell_day - buy_day
+    counter = 0
+
+    while len(graph) != 0:
+        while counter < len(graph):
+            if graph[0] - buy_day > money_made:
+                best_buy_day = buy_day
+                sell_day = graph[0]
+                money_made = graph[0] - buy_day
+            graph.append(graph.pop(0))
+            counter += 1
+        buy_day = graph.pop(0)
+        counter = 0
+    best_days.append(prices.index(best_buy_day))
+    best_days.append(prices.index(sell_day))
+
+    return best_days
